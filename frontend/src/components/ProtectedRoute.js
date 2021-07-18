cec73cbdf55adcefc62cedbb4ecd687041350a7e
tz17012016 <tz17012016@gmail.com>
2021-07-18 22:28:43 +0300

    fix1.0.1

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const isAuthenticated = localStorage.getItem('userInfo');
+  const userInfo = useSelector((state) => ({ ...state.userLogin.userInfo }));
+  const isAuthenticated = localStorage.getItem('userInfo') || userInfo;
 
   return (
     <Route
